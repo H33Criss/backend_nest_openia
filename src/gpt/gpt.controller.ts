3d127807ff6a47ae5c9a62fb0ad200b6fc582505
Cristobal-coding <agartkg95@gmail.com>
2024-03-12 19:37:04 -0300

    Implement text to audio function

--- src/gpt/gpt.controller.ts
+++ src/gpt/gpt.controller.ts
@@ -1,18 +1,51 @@
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { GptService } from './gpt.service';
-import { OrthographyDto, ProsConsDiscusserDto, TranslateDto } from './dtos';
-import { Response } from 'express';
+import {
+  OrthographyDto,
+  ProsConsDiscusserDto,
+  TextToAudioDto,
+  TranslateDto,
+} from './dtos';
+import type { Response } from 'express';
 
 @Controller('gpt')
 export class GptController {
   constructor(private readonly gptService: GptService) {}
 
+  @Post('text-to-audio')
+  async textToAudio(
+    @Body() textToAudioDto: TextToAudioDto,
+    @Res() res: Response,
+  ) {
+    const filePath = await this.gptService.textToAudio(textToAudioDto);
+    res.setHeader('Content-Type', 'audio/mp3');
+    res.status(HttpStatus.OK);
+    res.sendFile(filePath);
+  }
+  @Get('text-to-audio/:fileId')
+  async textToAudioGetter(
+    @Param('fileId') fileId: string,
+    @Res() res: Response,
+  ) {
+    const filePath = await this.gptService.textToAudioGetter(fileId);
+    res.setHeader('Content-Type', 'audio/mp3');
+    res.status(HttpStatus.OK);
+    res.sendFile(filePath);
+  }
   @Post('orthography-check')
-  orthographyCheck(@Body() orthographyDto: OrthographyDto) {
+  async orthographyCheck(@Body() orthographyDto: OrthographyDto) {
     return this.gptService.orthographyCheck(orthographyDto);
   }
   @Post('pros-cons-discusser')
-  prosConsDiscusser(@Body() prosConsDiscusserDto: ProsConsDiscusserDto) {
+  async prosConsDiscusser(@Body() prosConsDiscusserDto: ProsConsDiscusserDto) {
     return this.gptService.prosConsDiscusser(prosConsDiscusserDto);
   }
   @Post('translate')
--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,11 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import * as path from 'path';
+import * as fs from 'fs';
 import {
   orthographyCheckUseCase,
   prosConsDiscusserStreamUseCase,
   prosConsDiscusserUseCase,
+  textToAudioUseCase,
   translateUseCase,
 } from './use-cases';
-import { OrthographyDto, ProsConsDiscusserDto, TranslateDto } from './dtos';
+import {
+  OrthographyDto,
+  ProsConsDiscusserDto,
+  TextToAudioDto,
+  TranslateDto,
+} from './dtos';
 import OpenAI from 'openai';
 
 @Injectable()
@@ -14,6 +22,19 @@ export class GptService {
     apiKey: process.env.OPENAI_API_KEY,
   });
 
+  async textToAudio({ prompt, voice }: TextToAudioDto) {
+    return await textToAudioUseCase(this.openAi, { prompt, voice });
+  }
+  async textToAudioGetter(fileId: string): Promise<string> {
+    const filePath = path.resolve(
+      __dirname,
+      '../../generated/audios',
+      `${fileId}.mp3`,
+    );
+    const wasFound = fs.existsSync(filePath);
+    if (!wasFound) throw new NotFoundException(`File "${fileId}" not found.`);
+    return filePath;
+  }
   async orthographyCheck({ prompt }: OrthographyDto) {
     return await orthographyCheckUseCase(this.openAi, { prompt });
   }
